dd700e7f730edbb1b4b1579a74b943592c4f9db7
pandeemiC <lkooxer55@hotmail.com>
2025-05-02 16:24:43 +0300

    database+trending movies per query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDebounce } from "react-use";
+import { updateSearchCount, getTrendingMovies } from "./appwrite";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
@@ -19,6 +20,7 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
 
@@ -54,6 +56,10 @@ function App() {
       }
 
       setMovieList(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage("Error fetching movies. Please try again later.");
@@ -62,9 +68,22 @@ function App() {
     }
   };
 
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`);
+    }
+  };
+
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
 
   return (
     <main>
@@ -96,9 +115,21 @@ function App() {
             </h1>
             <Search search={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
-
+          {trendingMovies.length > 0 && (
+            <section className="trending">
+              <h2>Trending Movies</h2>
+              <ul>
+                {trendingMovies.map((movie, index) => (
+                  <li key={movie.$id}>
+                    <p>{index + 1}</p>
+                    <img src={movie.poster_url} alt={movie.title} />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          )}
           <section className="all-movies">
-            <h2 className="mt-[40px]">All Movies</h2>
+            <h2>All Movies</h2>
             {isLoading ? (
               <Spinner />
             ) : errorMessage ? (
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ const MovieCard = ({
   movie: { title, vote_average, poster_path, release_date, original_language },
 }) => {
   return (
-    <div className="movie-card">
+    <div className="movie-card cursor-pointer">
       <img
         src={
           poster_path
