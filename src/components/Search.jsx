bd196c68cc689aad1d29ae3637b5366116972bb5
pandeemiC <lkooxer55@hotmail.com>
2025-05-06 19:34:34 +0300

    added search on browse

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = ({ setSearchTerm, searchTerm, onSearchClick }) => {
+  const [placeholderText, setPlaceholderText] = useState(
+    "Search through 1000+ movies online..."
+  );
+
+  useEffect(() => {
+    const updatedPlaceholder = () => {
+      if (window.innerWidth >= 640) {
+        setPlaceholderText("Search through 1000+ movies online...");
+      } else {
+        setPlaceholderText("Search movies..");
+      }
+    };
+    updatedPlaceholder;
+    window.addEventListener("resize", updatedPlaceholder);
+
+    return () => {
+      window.removeEventListener("resize", updatedPlaceholder);
+    };
+  }, []);
+
   return (
     <div className="search">
       <div>
         <img src="./search.svg" alt="SearchSVG" />
         <input
           type="text"
-          placeholder="Search through 1000+ movies online..."
+          placeholder={placeholderText}
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           onKeyDown={(e) => {
--- src/pages/BrowsePage.jsx
+++ src/pages/BrowsePage.jsx
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Search from "../components/Search";
+import { useDebounce } from "react-use";
+import MovieCard from "../components/MovieCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
 
 function BrowsePage() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+
+  // useEffect(() => {
+  //   fetchMovies(debouncedSearchTerm);
+  // }, [debouncedSearchTerm]);
+
+  // const handleScrollMovies = () => {
+  //   if (allMoviesSectionRef.current) {
+  //     allMoviesSectionRef.current.scrollIntoView({
+  //       behavior: "smooth",
+  //       block: "start",
+  //     });
+  //   }
+  // };
+
   return (
-    <div className="wrapper px-4 py-8 min-h-screen">
-      <h1 className="text-gradient text-3xl">Browse All Movies</h1>
-      <p className="text-gray-400 text-center font-semibold text-xl">
-        (Under construction type sih..)
-      </p>
-    </div>
+    <main className="wrapper px-4 md:px-8 lg:px-12 py-8 min-h-screen">
+      <section className="gap-4 mb-8 all-movies">
+        <h2 className="text-gradient text-3xl md:text-4xl font-bold">
+          Browse All Movies
+        </h2>
+        <div className="flex flex-row items-center justify-start gap-4 sm:gap-6">
+          <button className="cursor-pointer inline-flex text-white bg-gradient font-semibold px-6 py-3 rounded-lg transition duration-300 ease-in-out transform hover:scale-[1.03] hover:shadow-lg hover:shadow-purple-500/30 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-primary focus:ring-[#AB8BFF]">
+            <FontAwesomeIcon
+              icon={faFilter}
+              className="transition-colors text-xl"
+            />
+            Filter
+          </button>
+          <Search
+            search={searchTerm}
+            setSearchTerm={setSearchTerm}
+
+            // onSearchClick={handleScrollMovies}
+          />
+        </div>
+      </section>
+    </main>
   );
 }
 
